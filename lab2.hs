171e78670dbb6b7f7887e6d1100f46ea283e2b8c
Alexandru Dinu <alex.dinu07@gmail.com>
2017-03-07 00:50:53 +0200

    fixed code

--- lab2.hs
+++ lab2.hs
@@ -1,11 +1,11 @@
-fact :: Int -> Int -> Int
---fact :: Integer -> Integer -> Integer
+-- Int will overflow, Integer will not
+
+--fact :: Int -> Int -> Int
+fact :: Integer -> Integer -> Integer
 fact acc 0 = acc
 fact acc n = fact (n * acc) (n - 1)
 
 
-
-mZipWith :: (a -> b -> c) -> [a] -> [b] -> [c]
 mZipWith f l [] = l
 mZipWith f [] l = l
 mZipWith f (e1:l1) (e2:l2) = (f e1 e2):(mZipWith f l1 l2)
@@ -13,10 +13,6 @@ mZipWith f (e1:l1) (e2:l2) = (f e1 e2):(mZipWith f l1 l2)
 
 -- [1,2,3] -> [[1], [1,2], [1,2,3]]
 f1 n l = if n > length l then [] else (take n l) : (f1 (n+1) l)
-
-
-
-
 
 
 -- function composition
@@ -37,8 +33,6 @@ h f g x = f (g x)
 f l = (.) (map (+1)) (filter even) l
 
 
-
-
 -- merge sort
 merge [] [] = []
 merge [] l = l
@@ -48,7 +42,3 @@ merge (e1:l1) (e2:l2) = if (e1 < e2) then e1:(merge l1 (e2:l2)) else e2:(merge (
 mergesort [] = []
 mergesort (e:[]) = (e:[])
 mergesort l = let mid = (div (length l) 2) in merge (mergesort (take mid l)) (mergesort (drop mid l))
-
-
-
-
